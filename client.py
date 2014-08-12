28f80e40c6e3842103974e804e4c6fe755f05795
Jamie Bowers <jtb56@uclive.ac.nz>
2014-08-12 21:26:15 +1200

    Fixed several minor bugs in client.py.

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import requests
 from Adafruit_BMP085 import BMP085
+import json
 
 #initialise sensor
 bmp = BMP085(0x77, 3)  # ULTRAHIRES Mode
@@ -10,4 +11,5 @@ pressure = bmp.readPressure()
 payload = {'temperature': temp, 'pressure': pressure}
 
 server_url = 'http://zephos.duckdns.org:5000/temperature_pressure'
-r = requests.post('server_url', data=json.dumps(payload))
+r = requests.post(server_url, data=json.dumps(payload))
+print(r.code)
