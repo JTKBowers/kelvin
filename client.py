dd0746db0449cc0fbacc3e193b8502196036238b
Jamie Bowers <jtb56@uclive.ac.nz>
2014-08-12 21:02:42 +1200

    Added basic client program which reads the temperature annd pressure and POSTs them to a url in json format.

---
+++ client.py
@@ -0,0 +1,13 @@
+import requests
+from Adafruit_BMP085 import BMP085
+
+#initialise sensor
+bmp = BMP085(0x77, 3)  # ULTRAHIRES Mode
+
+temp = bmp.readTemperature()
+pressure = bmp.readPressure()
+
+payload = {'temperature': temp, 'pressure': pressure}
+
+server_url = 'http://zephos.duckdns.org:5000/temperature_pressure'
+r = requests.post('server_url', data=json.dumps(payload))
