695a6a4ebbc84ea20e1ecf748146acfc96a69950
Jamie Bowers <jtb56@uclive.ac.nz>
2014-08-15 18:24:24 +1200

    Cast POST body string so that the json module can parse it.

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def temperature_data():
     for l in bottle.request.body:
         print (l)
     print (bottle.request.body)
-    data = json.loads(bottle.request.body)
+    data = json.loads(str(bottle.request.body))
     _temp = data["temperature"]
 
 @app.route('/temperature_pressure', method='GET')
