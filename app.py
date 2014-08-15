ab4b126f5b248a4375d27a02b6cef2708c3eb0f5
Jamie Bowers <jtb56@uclive.ac.nz>
2014-08-15 18:01:20 +1200

    Added simple temperature decoding and storage (of one value).

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
 import bottle
+import json
 
 app = application = bottle.Bottle()
 
+temp = 0
 @app.route('/temperature_pressure', method='POST')
 def index():
+    data = json.loads(request.body)
+    temp = data["temperature"]
     for l in request.body:
         print (l)
     print (request.body.readlines())
 
-@app.route('/')
+@app.route('/temperature_pressure', method='GET')
 def index():
-    #return bottle.template('<html><body>Hello World!</body></html>')
-    return '<html><body>Hello World2!</body></html>'
+    return bottle.template('<html><body>Temperature is: {{ temp }}</body></html>', temp=temp)
+
+
+# @app.route('/')
+# def index():
+#     #return bottle.template('<html><body>Hello World!</body></html>')
+#     return '<html><body>Hello World2!</body></html>'
 
 #debug stuff
 if __name__ == '__main__':
