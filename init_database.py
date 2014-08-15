e8154c4252943894624c6e731bd495311548452a
Jamie Bowers <jtb56@uclive.ac.nz>
2014-08-15 20:06:57 +1200

    Added script to initialise sensor database.

---
+++ init_database.py
@@ -0,0 +1,17 @@
+import sqlite3
+conn = sqlite3.connect('sensor_data.db')
+
+c = conn.cursor()
+
+# Create table
+c.execute('''CREATE TABLE temperature
+             (date text, temperature real)''')
+
+c.execute('''CREATE TABLE pressure
+             (date text, pressure real)''')
+
+conn.commit()
+
+# We can also close the connection if we are done with it.
+# Just be sure any changes have been committed or they will be lost.
+conn.close()
