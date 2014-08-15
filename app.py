b7e80424fe0b03dbe7a78aefe2759eac2b6044d8
Jamie Bowers <jtb56@uclive.ac.nz>
2014-08-15 18:19:33 +1200

    Fixed more merge conflict issues.

--- app.py
+++ app.py
@@ -3,18 +3,19 @@ import json
 
 app = application = bottle.Bottle()
 
+_temp = 0
 @app.route('/temperature_pressure', method='POST')
-def index():
+def temperature_data():
     data = json.loads(bottle.request.body)
-    temp = data["temperature"]
+    _temp = data["temperature"]
     for l in bottle.request.body:
         print (l)
     print (bottle.request.body.readlines())
 
-@app.route('/lol')
-def index():
+@app.route('/temperature_pressure', method='POST')
+def temperature_data():
     #return bottle.template('<html><body>Hello World!</body></html>')
-    return '<html><body>Hello World2!</body></html>'
+    return template('<html><body> Latest temperature is: {{temp}} </body></html>', temp=_temp)
 
 #debug stuff
 if __name__ == '__main__':
