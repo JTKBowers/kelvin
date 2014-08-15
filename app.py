6fc2a7535161fd696044604de60d0098ad8e8e60
Jamie Bowers <jtb56@uclive.ac.nz>
2014-08-15 20:12:50 +1200

    Added database storage and retrieval of one temperature value.

--- app.py
+++ app.py
@@ -2,20 +2,31 @@ import bottle
 import json
 
 app = application = bottle.Bottle()
+plugin = bottle.ext.sqlite.Plugin(dbfile='sensor_data.db')
+app.install(plugin)
 
-_temp = 0
-@app.route('/temperature_pressure', method='POST')
+@app.route('/temperature_pressure', db, method='POST')
 def temperature_data():
     for l in bottle.request.body:
         print (l)
     print (bottle.request.json)
     data = bottle.request.json
-    _temp = data["temperature"]
 
-@app.route('/temperature_pressure', method='GET')
-def temperature_data():
+
+    temp = data["temperature"]
+    
+    c.execute("INSERT INTO temperature VALUES ('2006-01-05',?)", (temp,))
+
+    pressure = data["pressure"]
+    c.execute("INSERT INTO pressure VALUES ('2006-01-05',?)", (pressure,))
+
+@app.route('/temperature', db, method='GET')
+def temperature_data(db):
     #return bottle.template('<html><body>Hello World!</body></html>')
-    return bottle.template('<html><body> Latest temperature is: {{temp}} </body></html>', temp=_temp)
+    temp = db.execute('SELECT * from temperature').fetchone()
+    if temp:
+        return bottle.template('<html><body> Latest temperature is: {{temp}} </body></html>', temp=temp)
+    return HTTPError(404, "Page not found")
 
 #debug stuff
 if __name__ == '__main__':
